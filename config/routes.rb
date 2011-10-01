04d426f020bb041513589877e778592c0f113965
Al Koury <al@alkoury.com>
2011-10-01 06:19:08 -0400

    Removed default Rails home page.

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 SampleApp::Application.routes.draw do
-  get "pages/home"
-
-  get "pages/contact"
-
-  get "pages/about"
   
-  get "pages/help"
+  root :to => 'pages#home'
+  
+  match '/contact', :to => 'pages#contact'
+  
+  match '/about',   :to => 'pages#about'
+  
+  match '/help',    :to => 'pages#help'
+  
 end
